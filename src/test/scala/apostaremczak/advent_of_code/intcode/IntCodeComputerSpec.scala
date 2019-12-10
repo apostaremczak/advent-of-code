e45b0bd251f30603cfbcf35ecef0b7210a53d58f
Angelika Postaremczak <angelikapostar@gmail.com>
2019-12-10 03:54:50 +0100

    Add final example from day 5 to tests

--- src/test/scala/apostaremczak/advent_of_code/intcode/IntCodeComputerSpec.scala
+++ src/test/scala/apostaremczak/advent_of_code/intcode/IntCodeComputerSpec.scala
@@ -40,5 +40,19 @@ class IntCodeComputerSpec extends AnyWordSpec with Matchers {
       computer.diagnosticCode(100) must equal(1)
       computer.diagnosticCode(8) must equal(1)
     }
+
+    "calculate diagnostic code of a larger program" in {
+      val computer = new IntCodeComputer(
+        IndexedSeq(
+          3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+          1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+          999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+        )
+      )
+
+      computer.diagnosticCode(0) must equal(999)
+      computer.diagnosticCode(8) must equal(1000)
+      computer.diagnosticCode(888) must equal(1001)
+    }
   }
 }
