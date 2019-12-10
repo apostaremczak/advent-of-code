0f594ac40cffb897f62717f5efb904741d105f92
Angelika Postaremczak <angelikapostar@gmail.com>
2019-12-10 17:25:38 +0100

    Return state when a program finishes

--- src/main/scala/apostaremczak/advent_of_code/intcode/IntCodeComputer.scala
+++ src/main/scala/apostaremczak/advent_of_code/intcode/IntCodeComputer.scala
@@ -6,22 +6,22 @@ import scala.annotation.tailrec
 
 class IntCodeComputer(memory: Memory) {
   @tailrec
-  private def readInstruction(state: State): (Memory, List[Int]) = {
+  private def readInstruction(state: State): State = {
     state.opCode.operation match {
-      case Halt      => (state.memory, state.outputs)
+      case Halt      => state
       case operation => readInstruction(operation.calculate(state))
     }
   }
 
-  def runProgram(computerInput: Int): (Memory, List[Int]) =
-    readInstruction(State(memory, input = List(computerInput)))
-
-  def runProgram(computerInput: List[Int]): (Memory, List[Int]) =
+  def runProgram(computerInput: List[Int]): State =
     readInstruction(State(memory, input = computerInput))
 
-  def diagnosticCode(computerInput: Int): Int =
-    runProgram(computerInput)._2.last
+  def runProgram(computerInput: Int): State =
+    runProgram(List(computerInput))
 
   def diagnosticCode(computerInput: List[Int]): Int =
-    runProgram(computerInput)._2.last
+    runProgram(computerInput).outputs.last
+
+  def diagnosticCode(computerInput: Int): Int =
+    diagnosticCode(List(computerInput))
 }
