8e0fcca1cb64c714efc8cc507be42cdb22bfcaf1
Angelika Postaremczak <angelikapostar@gmail.com>
2020-12-05 12:25:52 +0100

    Improve solution to day 5 with some binary magic, WHY DID NO ONE TELL ME ABOUT THIS

--- 2020-python/solutions/day_05_binary_boarding.py
+++ 2020-python/solutions/day_05_binary_boarding.py
@@ -1,35 +1,19 @@
 from typing import List
 
 
-def partition(low: int, high: int, directions: str) -> int:
-    if low == high:
-        return low
-    mid = low + (high - low) // 2
-    if directions[0] in ("F", "L"):
-        return partition(low, mid, directions[1:])
-    return partition(mid + 1, high, directions[1:])
-
-
 class BoardingPass:
-    ROW_RANGE = (0, 127)
-    COLUMN_RANGE = (0, 7)
-
     def __init__(self, seat_code: str):
-        row_code = seat_code[:7]
-        col_code = seat_code[7:]
-        self.row = partition(*BoardingPass.ROW_RANGE,
-                             directions=row_code)
-        self.column = partition(*BoardingPass.COLUMN_RANGE,
-                                directions=col_code)
-        self.seat_id = self.row * 8 + self.column
+        seat_code_binary = seat_code \
+            .replace("F", "0") \
+            .replace("L", "0") \
+            .replace("B", "1") \
+            .replace("R", "1")
+        self.seat_id = int(seat_code_binary, 2)
 
 
 def read_puzzle_input(input_file_path: str) -> List[BoardingPass]:
     with open(input_file_path, "r") as f:
-        return [
-            BoardingPass(x)
-            for x in f.readlines()
-        ]
+        return [BoardingPass(x) for x in f.readlines()]
 
 
 def part_1(boarding_passes: List[BoardingPass]) -> int:
@@ -39,9 +23,7 @@ def part_1(boarding_passes: List[BoardingPass]) -> int:
 def part_2(boarding_passes: List[BoardingPass]) -> int:
     seat_ids = {b_pass.seat_id for b_pass in boarding_passes}
     seat_id_range = set(range(min(seat_ids), max(seat_ids)))
-    missing_seats = seat_id_range.difference(seat_ids)
-    assert len(missing_seats) == 1
-    return missing_seats.pop()
+    return seat_id_range.difference(seat_ids).pop()
 
 
 if __name__ == '__main__':
