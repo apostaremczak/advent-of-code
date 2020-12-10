ee77866cf5e6bacfe9d4bad81d1c8fb2105991fb
Angelika Postaremczak <angelikapostar@gmail.com>
2020-12-10 14:56:39 +0100

    Make the solution to day 10 a little bit more readable

--- 2020-python/solutions/day_10_adapter_array.py
+++ 2020-python/solutions/day_10_adapter_array.py
@@ -1,5 +1,5 @@
 import re
-from typing import Set, List
+from typing import List
 
 
 def read_puzzle_input(input_file_path: str) -> List[int]:
@@ -9,11 +9,7 @@ def read_puzzle_input(input_file_path: str) -> List[int]:
 
 def get_joltage_diffs(adapters: List[int]) -> List[int]:
     adapters_sort = [0] + sorted(adapters) + [max(adapters) + 3]
-    return [
-        jolt - adapters_sort[i - 1]
-        for i, jolt in enumerate(adapters_sort)
-        if i >= 1
-    ]
+    return [y - x for x, y in zip(adapters_sort, adapters_sort[1:])]
 
 
 def part_1(adapters: List[int]) -> int:
@@ -24,14 +20,17 @@ def part_1(adapters: List[int]) -> int:
 def part_2(adapters: List[int]) -> int:
     diffs = get_joltage_diffs(adapters)
     diffs_char = "".join([str(d) for d in diffs])
+    # Places where one adapter can be used or not (2 possibilities)
     pow_2 = len(re.findall(r"(?=(3113))", diffs_char))
+    # Places where two adapters can be used or not (4 possibilities)
     pow_4 = len(re.findall(r"((?=(31113))|(^1113))", diffs_char))
+    # Places where three adapters can be used or not (7 possibilities)
     pow_7 = diffs_char.count("1111")
+    # Total number of adapter arrangements
     return 2 ** pow_2 * 4 ** pow_4 * 7 ** pow_7
 
 
 if __name__ == '__main__':
     puzzle_input = read_puzzle_input("../puzzle_inputs/day_10.csv")
-    print(sorted(puzzle_input))
     print(f"Solution to part 1: {part_1(puzzle_input)}")
     print(f"Solution to part 2: {part_2(puzzle_input)}")
