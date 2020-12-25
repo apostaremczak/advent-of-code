5b25ea2b5bfac3d2dd0f1d9e93b18dce8aa4388c
Angelika Postaremczak <angelikapostar@gmail.com>
2020-12-25 15:48:00 +0100

    Simplify finding discrete logarithms with SymPy

--- 2020-python/solutions/day_25_combo_breaker.py
+++ 2020-python/solutions/day_25_combo_breaker.py
@@ -1,3 +1,4 @@
+from sympy.ntheory.residue_ntheory import discrete_log
 from typing import Tuple
 
 
@@ -6,30 +7,9 @@ def read_puzzle_input(input_file_path: str) -> Tuple[int]:
         return tuple(int(x) for x in f.readlines())
 
 
-def determine_loop_size(subject_number: int, public_key: int) -> int:
-    """
-    Pohlig-Hellman would be nicer for finding discrete logarithms though
-    """
-    current_value = 1
-    loop_size = 0
-    while current_value != public_key:
-        current_value = (current_value * subject_number) % 20201227
-        loop_size += 1
-    return loop_size
-
-
-def transform_subject_number(subject_number: int,
-                             loop_size: int,
-                             current_value: int = 1) -> int:
-    for _ in range(loop_size):
-        current_value = (current_value * subject_number) % 20201227
-    return current_value
-
-
 def part_1(card_public_key: int, door_public_key: int) -> int:
-    card_loop_size = determine_loop_size(7, card_public_key)
-    card_encryption_key = transform_subject_number(door_public_key,
-                                                   card_loop_size)
+    card_loop_size = discrete_log(20201227, card_public_key, 7)
+    card_encryption_key = pow(door_public_key, card_loop_size, 20201227)
     return card_encryption_key
 
 
