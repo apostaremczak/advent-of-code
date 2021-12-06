f825ec1400fe9423d1f7de519365c0b1a9e388ac
Angelika Postaremczak <angelikapostar@gmail.com>
2021-12-06 22:44:57 +0100

    Add Day 21: Allergen Assessment

--- 2020-python/solutions/day_21_allergen_assessment.py
+++ 2020-python/solutions/day_21_allergen_assessment.py
@@ -1,39 +1,67 @@
 import re
-from typing import List, Tuple, Set
+from collections import Counter, defaultdict
+from typing import Dict, List, Tuple, Set
+
+Ingredient = str
+Allergen = str
+Food = Tuple[List[Ingredient], List[Allergen]]
 
 
-def read_puzzle_input(input_file_path: str) -> List[Tuple[str, str]]:
-    ingredient_lists = []
+def read_puzzle_input(input_file_path: str) -> List[Food]:
+    food_list = []
     with open(input_file_path, "r") as f:
         for line in f.readlines():
-            matches = re.findall(r"^(.*) \(contains (.*)\)$", line)
-            ingredient_lists.extend(matches)
-    return ingredient_lists
+            matches = re.match(r"^(.*) \(contains (.*)\)$", line).groups()
+            ingredients_str, allergens_str = matches
+            food_list.append((ingredients_str.split(),
+                              allergens_str.split(", ")))
+    return food_list
 
 
-def find_allergens(entries: List[Tuple[str, str]]) -> Set[str]:
-    unique_allergens = set()
+def get_potentially_allergic_ingredients(food_list: List[Food]) -> Dict[Allergen, Set[Ingredient]]:
     allergen_ingredients = {}
-    for ingredients_str, allergens_str in entries:
-        ingredient_allergens = set(allergens_str.split(", "))
-        unique_allergens = unique_allergens.union(ingredient_allergens)
-        ingredients = ingredients_str.split(" ")
-        for al in ingredient_allergens:
-            al_i = allergen_ingredients.get(al, [])
-            al_i.append(ingredients)
-            allergen_ingredients[al] = al_i
+    for ingredients, allergens in food_list:
+        for allergen in allergens:
+            allergen_ingredients[allergen] = allergen_ingredients.get(allergen, set(ingredients)) & set(ingredients)
+    return allergen_ingredients
 
 
+def part_1(food_list: List[Food]) -> int:
+    ingredient_counts = Counter()
+    for ingredients, _ in food_list:
+        ingredient_counts.update(ingredients)
 
-def part_1(entries: List[Tuple[str, str]]) -> int:
-    pass
+    allergen_ingredients = get_potentially_allergic_ingredients(food_list)
+    unsafe_ingredients = set.union(*[v for v in allergen_ingredients.values()])
+    return sum(
+        count
+        for ingredient, count in ingredient_counts.items()
+        if ingredient not in unsafe_ingredients
+    )
 
 
-def part_2(entries: List[Tuple[str, str]]) -> int:
-    pass
+def part_2(food_list: List[Food]) -> str:
+    potential_allergens = get_potentially_allergic_ingredients(food_list)
+    canonical_allergens: Dict[Allergen, Ingredient] = {}
+
+    while potential_allergens:
+        allergen, ingredients = min(potential_allergens.items(),
+                                    key=lambda x: len(x[1]))
+        potential_allergens = {
+            a: i.difference(ingredients)
+            for a, i in potential_allergens.items()
+        }
+        potential_allergens.pop(allergen)
+
+        ingredient = ingredients.pop()
+        canonical_allergens[allergen] = ingredient
+
+    return ",".join([
+        ingredient for _, ingredient in sorted(canonical_allergens.items())
+    ])
 
 
 if __name__ == '__main__':
-    puzzle_input = read_puzzle_input("../puzzle_inputs/day_xx.txt")
+    puzzle_input = read_puzzle_input("../puzzle_inputs/day_21.txt")
     print(f"Solution to part 1: {part_1(puzzle_input)}")
     print(f"Solution to part 2: {part_2(puzzle_input)}")
--- 2020-python/test/test_day_21_allergen_assessment.py
+++ 2020-python/test/test_day_21_allergen_assessment.py
@@ -1,4 +1,11 @@
 from solutions.day_21_allergen_assessment import *
 
-if __name__ == '__main__':
-    print(read_puzzle_input("puzzle_inputs/day_21.txt"))
+food_list = read_puzzle_input("puzzle_inputs/day_21.txt")
+
+
+def test_part_1():
+    assert part_1(food_list) == 5
+
+
+def test_part_2():
+    assert part_2(food_list) == "mxmxvkd,sqjhc,fvjkl"
