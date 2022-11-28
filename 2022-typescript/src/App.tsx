26d7b11b8f42123aba06af4537f1531e651ce923
Angelika Postaremczak <angelika.postaremczak@allegro.pl>
2022-11-28 22:32:35 +0100

    Christmas gallery template

--- 2022-typescript/src/App.tsx
+++ 2022-typescript/src/App.tsx
@@ -1,15 +1,140 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function App() {
+function Copyright() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-
-        </p>
-      </header>
-    </div>
+    <Typography variant="body2" color="text.secondary" align="center">
+      {/* {'Copyright © '} */}
+      {'Gallery template by '}
+      <Link color="inherit" href="https://mui.com/">
+        MUI.com
+      </Link>{' '}
+    </Typography>
   );
 }
 
-export default App;
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
+
+export default function Album() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Advent of Code 2022
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              🎄Advent of Code🎄 2022
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              Since I know absolutely nothing about web dev, I've decided to motivate myself
+              for this year of AoC by learning TypeScript and displaying all my puzzle solutions
+              on a simple website. Enjoy!
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained" href="https://github.com/apostaremczak/advent-of-code">Source code on GitHub</Button>
+              <Button variant="outlined" href='https://adventofcode.com/2022'>Advent of Code puzzles</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 6 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      // Spacing above a photo
+                      pt: '0%',
+                    }}
+                    image="https://notesfrompoland.com/wp-content/uploads/2021/12/Christmas-Tree-Snow-drawing-980x551.jpg"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          ☃️
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          {'Copyright © '}
+          <Link color="inherit" href="https://www.linkedin.com/in/apostaremczak/">
+            Angelika Postaremczak
+          </Link>
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
