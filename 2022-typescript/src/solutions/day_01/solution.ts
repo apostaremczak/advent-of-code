08271f28bc60957af1b6dcde5321a97d683bcc07
Angelika Postaremczak <angelika.postaremczak@allegro.pl>
2022-12-01 23:29:55 +0100

    Idk

--- 2022-typescript/src/solutions/day_01/solution.ts
+++ 2022-typescript/src/solutions/day_01/solution.ts
@@ -1,27 +1,13 @@
-import { readFileSync } from 'fs';
 import { join } from 'path';
+import { readSplitByDoubleEmpty } from '../../utils/readFile'
+import { sum, sortDesc } from '../../utils/arrays'
 
-function readFile(filename: string): string {
-    const result = readFileSync(join(__dirname, filename), 'utf-8');
-    return result;
-}
 
-function readSplitByElf(filename: string): string[] {
-    return readFile(filename).split("\n\n");
-}
+let elfs: string[] = readSplitByDoubleEmpty(join(__dirname, "01.txt"));
 
-function sortDesc(nums: number[]): number[] {
-    return nums.sort((one, two) => (one > two ? -1 : 1));
-}
-
-function sum(nums: number[]): number {
-    return nums.reduce((sum, current) => sum + current, 0)
-}
-
-let elfs = readSplitByElf("01.txt");
-
-let elfSnacks = elfs.map(e => e.split("\n").reduce((sum, current) => sum + parseInt(current), 0));
+let elfSnacks = elfs.map(e => e.split("\n").reduce((s, c) => s + parseInt(c), 0));
 console.log(Math.max(...elfSnacks));
 
 let topElfSnacks = sortDesc(elfSnacks).slice(0, 3)
 console.log(sum(topElfSnacks));
+
