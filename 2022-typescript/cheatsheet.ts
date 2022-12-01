f903afb5578ee9fc7a715b785236bdb5445194d6
Angelika Postaremczak <angelika.postaremczak@allegro.pl>
2022-12-01 09:02:16 +0100

    Start with day 1

--- 2022-typescript/cheatsheet.ts
+++ 2022-typescript/cheatsheet.ts
@@ -172,3 +172,40 @@ render(customer?.birthday);
 let log: any = (m: string) => render(m);
 let nullLog: any = null;
 nullLog?.('a');
+
+
+// Reading files
+
+import { readFileSync, promises as fsPromises } from 'fs';
+import { join } from 'path';
+
+// ✅ read file SYNCHRONOUSLY
+function syncReadFile(filename: string) {
+    const result = readFileSync(join(__dirname, filename), 'utf-8');
+
+    console.log(result);
+    return result;
+}
+
+syncReadFile('00.txt');
+
+// --------------------------------------------------------------
+
+// ✅ read file ASYNCHRONOUSLY
+async function asyncReadFile(filename: string) {
+    try {
+        const result = await fsPromises.readFile(
+            join(__dirname, filename),
+            'utf-8',
+        );
+
+        console.log(result);
+
+        return result;
+    } catch (err) {
+        console.log(err);
+        return 'Something went wrong'
+    }
+}
+
+asyncReadFile('00.txt');
