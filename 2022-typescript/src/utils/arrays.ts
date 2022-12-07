d80802c07eb868cfb5e634c1f945f78a1aced77f
Angelika Postaremczak <angelika.postaremczak@allegro.pl>
2022-12-07 13:22:03 +0100

    Move array utils to a separate script

---
+++ 2022-typescript/src/utils/arrays.ts
@@ -0,0 +1,7 @@
+export function sortDesc(nums: number[]): number[] {
+    return nums.sort((one, two) => (one > two ? -1 : 1));
+}
+
+export function sum(nums: number[]): number {
+    return nums.reduce((sum, current) => sum + current, 0);
+}
