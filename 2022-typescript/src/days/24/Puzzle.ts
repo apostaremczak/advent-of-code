9171a2c1ade8856a0279e6a22601eace630ffaab
Angelika Postaremczak <angelikapostar@gmail.com>
2023-12-01 09:24:44 +0100

    2022 last touches

---
+++ 2022-typescript/src/days/24/Puzzle.ts
@@ -0,0 +1,23 @@
+import Puzzle from '../../types/AbstractPuzzle';
+
+export default class ConcretePuzzle extends Puzzle {
+  public solveFirst(input: string): string {
+    // WRITE SOLUTION FOR TEST 1
+    return 'day 1 solution 1';
+  }
+
+  public getFirstExpectedResult(): string {
+    // RETURN EXPECTED SOLUTION FOR TEST 1;
+    return 'day 1 solution 1';
+  }
+
+  public solveSecond(input: string): string {
+    // WRITE SOLUTION FOR TEST 2
+    return 'day 1 solution 2';
+  }
+
+  public getSecondExpectedResult(): string {
+    // RETURN EXPECTED SOLUTION FOR TEST 2;
+    return 'day 1 solution 2';
+  }
+}
