6f33524b0d1aa2fe8f8bfd5079dc4bd73242cd25
Angelika Postaremczak <angelika.postaremczak@allegro.com>
2024-12-07 02:16:04 +0100

    Day 6

--- 2024-java/src/main/java/pl/apostaremczak/aoc/Day06.java
+++ 2024-java/src/main/java/pl/apostaremczak/aoc/Day06.java
@@ -1,18 +1,145 @@
 package pl.apostaremczak.aoc;
 
-public class Day06 extends PuzzleSolution {
+import pl.apostaremczak.aoc.util.Coord2D;
+import pl.apostaremczak.aoc.util.DirectionSupport;
+
+import java.util.*;
+
+public class Day06 extends PuzzleSolution implements DirectionSupport {
+    final private Coord2D startingPosition;
+    final private Coord2D startingDirection;
+    final private HashSet<Coord2D> Obstacles = new HashSet<>();
+    final private Integer MaxRow;
+    final private Integer MaxColumn;
+
     public Day06(String inputFilename) {
         super(inputFilename);
+
+        // Parse the input map; only keep track of the obstacle locations, since everything else is empty
+        Coord2D position = null;
+        Coord2D direction = null;
+        this.MaxRow = this.inputLines.length - 1;
+        this.MaxColumn = this.inputLines[0].length() - 1;
+
+        for (int rowIndex = 0; rowIndex <= MaxRow; rowIndex++) {
+            char[] row = this.inputLines[rowIndex].toCharArray();
+            for (int colIndex = 0; colIndex <= MaxColumn; colIndex++) {
+                switch (row[colIndex]) {
+                    case '#':
+                        Obstacles.add(new Coord2D(rowIndex, colIndex));
+                        break;
+                    case '^':
+                        position = new Coord2D(rowIndex, colIndex);
+                        direction = DirectionSupport.TOP;
+                        break;
+                }
+            }
+        }
+        assert position != null && direction != null : "Start position and direction not found";
+
+        this.startingPosition = position;
+        this.startingDirection = direction;
     }
 
     @Override
     public Long solvePart1() {
-        return 0L;
+        HashSet<Coord2D> visitedSpots = new HashSet<>();
+        Coord2D position = startingPosition;
+        Coord2D direction = startingDirection;
+
+        while (isWithinMapBounds(position)) {
+            visitedSpots.add(position);
+            Coord2D nextPosition = position.add(direction);
+            if (Obstacles.contains(nextPosition)) {
+                // Rotate right to avoid bumping into objects
+                direction = direction.rotateRightAngle(-90);
+                nextPosition = position.add(direction);
+            }
+            position = nextPosition;
+        }
+
+        return (long) visitedSpots.size();
     }
 
     @Override
     public Long solvePart2() {
-        return 0L;
+        HashSet<Coord2D> visitedSpots = new HashSet<>();
+        Coord2D position = startingPosition;
+        Coord2D direction = startingDirection;
+        HashSet<Coord2D> guardTraps = new HashSet<>();
+
+        // For each spot currently occupied by the guard, try placing an obstacle at their next position and check if they could get stuck in a loop
+        while (isWithinMapBounds(position) && !Obstacles.contains(position)) {
+            visitedSpots.add(position);
+            Coord2D nextPosition = position.add(direction);
+
+            // When an obstacle is encountered, turn right until the path is clear
+            if (Obstacles.contains(nextPosition)) {
+                HashSet<Coord2D> exploredNextPositions = new HashSet<>(List.of(nextPosition));
+                boolean foundNextSpot = false;
+                while (!foundNextSpot) {
+                    direction = direction.rotateRightAngle(-90);
+                    nextPosition = position.add(direction);
+                    exploredNextPositions.add(nextPosition);
+                    if (!Obstacles.contains(nextPosition)) {
+                        foundNextSpot = true;
+                    }
+                    if (exploredNextPositions.size() == 4) {
+                        throw new RuntimeException("Stuck in a single spot");
+                    }
+                }
+            }
+
+            // Try placing an obstacle at the next position, but only if it's not a spot that was previously visited
+            // (otherwise the guard couldn't have passed it to get to the current spot)
+            if (!visitedSpots.contains(nextPosition) && willTrapGuard(position, direction, nextPosition)) {
+                guardTraps.add(nextPosition);
+            }
+            position = nextPosition;
+        }
+        return (long) guardTraps.size();
+    }
+
+    public boolean willTrapGuard(Coord2D guardPosition, Coord2D guardDirection, Coord2D obstaclePosition) {
+        List<Coord2D> turns = new ArrayList<>();
+        Coord2D position = guardPosition;
+        Coord2D direction = guardDirection;
+        HashSet<Coord2D> obstacles = new HashSet<>(Obstacles);
+        obstacles.add(obstaclePosition);
+
+        while (isWithinMapBounds(position)) {
+            Coord2D nextPosition = position.add(direction);
+            if (obstacles.contains(nextPosition)) {
+                // We've already turned right at this position, so we've got a loop
+                if (turns.contains(position)) {
+                    return true;
+                }
+
+                // Turn right until the path is clear
+                HashSet<Coord2D> exploredNextPositions = new HashSet<>(List.of(nextPosition));
+                boolean foundNextSpot = false;
+                while (!foundNextSpot) {
+                    direction = direction.rotateRightAngle(-90);
+                    nextPosition = position.add(direction);
+                    exploredNextPositions.add(nextPosition);
+                    if (!obstacles.contains(nextPosition)) {
+                        foundNextSpot = true;
+                        // Take note of when we made this turn to detect loops
+                        turns.add(position);
+                    }
+                    if (exploredNextPositions.size() == 4 && !foundNextSpot) {
+                        // Single-spot loop
+                        return true;
+                    }
+                }
+            }
+            position = nextPosition;
+        }
+        return false;
+    }
+
+    private boolean isWithinMapBounds(Coord2D position) {
+        return position.row() >= 0 && position.column() >= 0 && position.row() <= MaxRow && position.column() <= MaxColumn;
     }
 
     public static void main(String[] args) {
@@ -21,5 +148,37 @@ public class Day06 extends PuzzleSolution {
         System.out.println("Part 1: " + part1Solution);
         Long part2Solution = day06.solvePart2();
         System.out.println("Part 2: " + part2Solution);
+    }
+
+    /**
+     * Used for debugging; prints a visualization of the current state of the map.
+     */
+    private void printMap(HashSet<Coord2D> obstacles, Coord2D guardPosition, Coord2D guardDirection) {
+        char guardMarker;
+        if (guardDirection.equals(TOP)) {
+            guardMarker = '^';
+        } else if (guardDirection.equals(RIGHT)) {
+            guardMarker = '>';
+        } else if (guardDirection.equals(BOTTOM)) {
+            guardMarker = 'v';
+        } else if (guardDirection.equals(LEFT)) {
+            guardMarker = '<';
+        } else {
+            throw new IllegalArgumentException("Illegal guard direction: " + guardDirection);
+        }
+
+        for (int row = 0; row <= MaxRow; row++) {
+            for (int col = 0; col <= MaxColumn; col++) {
+                Coord2D current = new Coord2D(row, col);
+                if (obstacles.contains(current)) {
+                    System.out.print('#');
+                } else if (guardPosition.equals(current)) {
+                    System.out.print(guardMarker);
+                } else {
+                    System.out.print('.');
+                }
+            }
+            System.out.print('\n');
+        }
     }
 }
--- 2024-java/src/test/java/pl/apostaremczak/aoc/Day06Tests.java
+++ 2024-java/src/test/java/pl/apostaremczak/aoc/Day06Tests.java
@@ -1,21 +1,72 @@
 package pl.apostaremczak.aoc;
 
 import org.junit.jupiter.api.Test;
+import pl.apostaremczak.aoc.util.Coord2D;
+import pl.apostaremczak.aoc.util.DirectionSupport;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class Day06Tests {
-    private final Day06 day06 = new Day06("src/test/resources/06.txt");
-
+public class Day06Tests implements DirectionSupport {
     @Test
     public void testSolvePart1() {
+        Day06 day06 = new Day06("src/test/resources/06.txt");
         Long result = day06.solvePart1();
-        assertEquals(0L, result);
+        assertEquals(41L, result);
+    }
+
+    @Test
+    public void testWillTrapGuard() {
+        Day06 day06 = new Day06("src/test/resources/06.txt");
+        Coord2D guardPosition = new Coord2D(6, 4);
+        Coord2D guardDirection = TOP;
+        Coord2D obstaclePosition = new Coord2D(6, 3);
+        boolean result = day06.willTrapGuard(guardPosition, guardDirection, obstaclePosition);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testWillTrapGuard2() {
+        Day06 day06 = new Day06("src/test/resources/06.txt");
+        Coord2D guardPosition = new Coord2D(8, 4);
+        Coord2D guardDirection = TOP;
+        Coord2D obstaclePosition = new Coord2D(8, 3);
+        boolean result = day06.willTrapGuard(guardPosition, guardDirection, obstaclePosition);
+        assertTrue(result);
     }
 
     @Test
     public void testSolvePart2() {
+        Day06 day06 = new Day06("src/test/resources/06.txt");
         Long result = day06.solvePart2();
+        assertEquals(6L, result);
+    }
+
+    @Test
+    public void testSolvePart2AdditionalInput() {
+        Day06 day6A = new Day06("src/test/resources/06_2.txt");
+        Long result = day6A.solvePart2();
+        assertEquals(1L, result);
+    }
+
+    @Test
+    public void testSolvePart3AdditionalInput() {
+        Day06 day6A = new Day06("src/test/resources/06_3.txt");
+        Long result = day6A.solvePart2();
+        assertEquals(1L, result);
+    }
+
+    @Test
+    public void testSolvePart4AdditionalInput() {
+        Day06 day6A = new Day06("src/test/resources/06_4.txt");
+        Long result = day6A.solvePart2();
+        assertEquals(1L, result);
+    }
+
+    @Test
+    public void testSolvePart5AdditionalInput() {
+        Day06 day6A = new Day06("src/test/resources/06_5.txt");
+        Long result = day6A.solvePart2();
         assertEquals(0L, result);
     }
 }
